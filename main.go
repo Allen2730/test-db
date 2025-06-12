35982be5b19ded629ea995b0f5aa653d2d5c6e38
Allen2730 <1051072440@qq.com>
2025-06-12 18:04:00 +0800

    initial

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	db_password := os.Getenv("DB_PASSWORD")
 	db_host := os.Getenv("DB_HOST")
 	db_port := os.Getenv("DB_PORT")
-	db_name := os.Getenv("DB_NAME")
+	db_name := "test"
 
 	dsn := db_user + ":" + db_password + "@tcp(" + db_host + ":" + db_port + ")/" + db_name + "?charset=utf8mb4&parseTime=True&loc=Local"
 
